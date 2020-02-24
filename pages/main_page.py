4439c4131698671c89b8809dcea27f48c23b300e
Igor Kiselev <igorkiselev93@gmail.com>
2020-02-24 16:03:55 +0300

    Correct mistakes with import from lesson 4.2 step 4

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,10 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+    
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
