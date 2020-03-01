9eba35293c7406174c0a02cb203011c21023fa2b
Igor Kiselev <igorkiselev93@gmail.com>
2020-03-01 14:35:57 +0300

    Make lesson 4.3 step 3

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,12 +7,18 @@ class ProductPage(BasePage):
         add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
     
-    def should_be_correct_product_name_in_basket(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+    def get_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        return product_name.text
+
+    def get_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return product_price.text
+    
+    def should_be_correct_product_name_in_basket(self, product_name):
         product_name_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET).text
         assert product_name == product_name_in_basket, "Product name doesn't match with product name in basket"
 
-    def should_be_correct_basket_price(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+    def should_be_correct_basket_price(self, product_price):
         basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
         assert product_price == basket_price, "Product price doesn't match with basket price"
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link) 
     page.open()
     page.click_on_add_to_basket_button()
     page.solve_quiz_and_get_code()
+    product_name = page.get_product_name()
+    product_price = page.get_product_price()
+    page.should_be_correct_product_name_in_basket(product_name)
+    page.should_be_correct_basket_price(product_price)
