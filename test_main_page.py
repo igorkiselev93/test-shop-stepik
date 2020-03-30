214a4baa48a8d99fbfa6c2ce3cd1507121437012
Igor Kiselev <igorkiselev93@gmail.com>
2020-03-30 20:26:51 +0300

    Make lesson 4.3 step 11

--- test_main_page.py
+++ test_main_page.py
@@ -3,21 +3,21 @@ from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 import pytest
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 @pytest.mark.skip
 @pytest.mark.login_page
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,23 @@ from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 import pytest
 
+class TestUserAddToBasketFromProductPage:
+    def test_user_can_add_product_to_basket(browser, link):
+        page = ProductPage(browser, link) 
+        page.open()
+        page.click_on_add_to_basket_button()
+        page.solve_quiz_and_get_code()
+        product_name = page.get_product_name()
+        product_price = page.get_product_price()
+        page.should_be_correct_product_name_in_basket(product_name)
+        page.should_be_correct_basket_price(product_price)
+
+    def test_user_cant_see_success_message_after_adding_product_to_basket(browser, link):
+        page = ProductPage(browser, link) 
+        page.open()
+        page.click_on_add_to_basket_button()
+        page.should_not_be_success_message() 
+
 @pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
